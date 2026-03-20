c4cd6d44a3154f5e33d74cc3117c6855c2907696
TigerSpade <cantilangalexandramarie@gmail.com>
2026-03-20 18:18:06 +0800

    hinayupak

--- main.py
+++ main.py
@@ -2733,4 +2733,4 @@ def rss_feed():
 
 if __name__ == '__main__':
     print("[DEBUG] [MAIN] Starting Flask app loop via main block...")
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True)
